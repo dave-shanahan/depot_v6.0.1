e3a04dcd8b81ace4dfab504aaf89db1607070643
David Shanahan <dshanahan@yozu.co.uk>
2023-02-13 08:34:10 +0000

    Iteration L3: Responding with Rich Text

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,20 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+
   resources :users
   resources :products do
     get :who_bought, on: :member
   end
 
-  scope '(:locale)' do
-    resources :orders
-    resources :line_items
-    resources :carts
-    root 'store#index', as: 'store_index'
-  end
+  resources :support_requests, only: [:index, :update]
+
+
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
